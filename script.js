1e034d8c4f67ba8a9ca13dc8cb9a00499842c712
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-03-17 19:50:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width=width;
+		this._height=height;
+	}
+	get width(){
+		return this._width;
+	}
+	get height(){
+		return this.height;
+	}
+	getArea(){
+		console.log("The are of"+this.width+"Rectangle"+this.height);
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	console.log(`${this.width}`);
+	console.log(`${this.height}`);
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
